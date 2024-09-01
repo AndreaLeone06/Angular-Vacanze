267e42ef348e316d779caf8a0cf08aad14793f75
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2024-09-01 09:51:21 +0000

    punto 4

--- angular-routing/src/app/spotify.service.ts
+++ angular-routing/src/app/spotify.service.ts
@@ -19,4 +19,9 @@ export class SpotifyService {
     const headers = new HttpHeaders({Authorization: environment.oauthToken});
     return this.http.get(url, { headers });
   }
+  getArtistByID(id: string) {
+    const url = 'https://api.spotify.com/v1/artists/${id}';
+    const headers = new HttpHeaders({Authorization: environment.oauthToken});
+    return this.http.get(url, { headers });
+  }
 }
