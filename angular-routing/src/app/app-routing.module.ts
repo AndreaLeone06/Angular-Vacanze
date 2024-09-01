e510944d6f5d7e9134498d82762289989a0a2399
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2024-09-01 09:57:46 +0000

    punto 6

--- angular-routing/src/app/app-routing.module.ts
+++ angular-routing/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TrackComponent } from './track/track.component';
 import { SearchComponent } from './search/search.component';
 import { AboutComponent } from './about/about.component';
+import { ArtistComponent } from './artist/artist.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'search', component: SearchComponent },
   { path: 'tracks/:id', component: TrackComponent },
   { path: '',   redirectTo: '/search', pathMatch: 'full' },
+  { path: 'artists/:id', component: ArtistComponent }
 ];
 
 @NgModule({
